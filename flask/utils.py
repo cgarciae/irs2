e55d87b1b7647bd4c6871d077ca455bed529d227
Cristian Garcia <cgarcia.e88@gmail.com>
2017-09-11 01:05:10 -0700

    save

--- flask/utils.py
+++ flask/utils.py
@@ -2,6 +2,15 @@ from werkzeug import Response
 import json
 
 
+class MongoJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        elif isinstance(obj, ObjectId):
+            return unicode(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
 def jsonify(*args, **kwargs):
     """ jsonify with support for MongoDB ObjectId
     """
