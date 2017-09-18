0cb662138f81521b4e51dd6fd9acbbcbd73aa955
Moises Vargas M <moisesweb@gmail.com>
2017-09-18 10:40:27 -0500

    tags template

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -2,12 +2,20 @@ class HomeController < ApplicationController
   def index ; end
   def brief ; end
   def hello ; end
+
   def search
     if params[:boton] == "searched"
       data = session[:data] || {}
       data[:category] = params[:category]
       data[:industry] = params[:industry]
       session[:data] = data
+      redirect_to :tags
     end
+
+    
+  end
+
+  def tags
+
   end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'intuitive-brief', to: 'home#brief'
   get 'hello', to: 'home#hello'
   get 'search', to: 'home#search'
+  get 'tags', to: 'home#tags'
   root :to => "home#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
