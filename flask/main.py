f90d353b3c1a78c5b0ec21c585b499302caca1a6
Moises Vargas M <moisesweb@gmail.com>
2017-10-15 10:03:10 -0500

    designers similarity

--- flask/main.py
+++ flask/main.py
@@ -61,6 +61,13 @@ IMAGES = (
     .load()
 ).cache()
 
+IMAGES_DESIGNERS = (
+    spark.read
+    .format("com.mongodb.spark.sql.DefaultSource")
+    .option("uri", "mongodb://mongo/brandon.designers")
+    .load()
+).cache()
+
 
 @app.route('/random-samples')
 def random_samples():
@@ -106,6 +113,37 @@ def get_similar(id):
 
     return jsonify(data = selected_images)
 
+
+@app.route('/designers/<id>')
+def get_similar(id):
+    intial_radius = float(request.args.get('initial-radius', 100))
+    radius = float(request.args.get('radius', 80))
+    n = int(request.args.get('n', 10))
+
+    db = mongo.db
+
+    img = db.images.find_one({'_id': ObjectId(id) })
+    target = img["embeddings"]
+
+    selected_images = (
+        IMAGES_DESIGNERS.rdd.map(lambda row: Row(
+            _id = row._id,
+            filename = row.filename,
+            distance = euclidean(row.embeddings, target)
+        ))
+        .toDF()
+        .where(F.col("distance") <= radius )
+        .orderBy("distance", ascending = True)
+        .withColumn("similarity", (F.lit(1.0) - (F.col("distance") / F.lit(intial_radius))) * F.lit(100.0) )
+        .collect()
+    )
+    selected_images = selected_images[1:]
+    shuffle(selected_images)
+    selected_images = selected_images[:n]
+    selected_images = sorted(selected_images, key = lambda row: row.distance)
+    selected_images = [ r.asDict() for r in selected_images ]
+
+    return jsonify(data = selected_images)
 
 
 if __name__ == '__main__':
